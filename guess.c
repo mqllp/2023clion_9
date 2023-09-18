c36a30a714c3e36231e7b56ef53e7bfa6ab5567a
mqllp_ <231250190@smail.nju.edu.cn>
2023-09-19 00:04:31 +0800

    1-intro

---
+++ guess.c
@@ -0,0 +1,15 @@
+//
+// Created by asus on 2023/9/18.
+//
+#include <stdio.h>
+int main(void)
+{
+    int a ;
+    printf("number a:");
+    scanf("%d",&a);
+    int b ;
+    printf("number b:");
+    scanf("%d",&b);
+    printf("the mum:%d",a+b);
+    return 0;
+}
--- hello.c
+++
@@ -1,9 +0,0 @@
-//
-// Created by asus on 2023/9/18.
-//
-#include <stdio.h>
-int main (void)
-{
-    printf("hello,world!");
-    return 0;
-}
